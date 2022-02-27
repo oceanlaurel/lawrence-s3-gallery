3f2a18162e64115d0b6ff022d5b57f3758067b4f
Lawrence Wong <oceandeep@gmail.com>
2022-02-27 15:14:50 +0800

    Padding for first item implemented

--- public/common/js/lawrence/tabbis-initializer.js
+++ public/common/js/lawrence/tabbis-initializer.js
@@ -46,10 +46,22 @@ function genTabbis(json, type) {
 		}
 	}
 
+	tabbisDataTabsArray = tuneTabbisStructure(tabbisDataTabsArray);
+
+console.log('tabbisDataTabsArray: '+tabbisDataTabsArray);
+console.log('here');
+
+
+	for (i = 0; i < tabbisDataTabsArray.length; i++) {
+		console.log('Level ' + i + ' folder list: ' + tabbisDataTabsArray[i]);
+	}
+
 	tabbis = '';
 	for (i = 0; i < tabbisDataTabsArray.length; i++) {
 		tabbis += tabbisDataTabsArray[i];
 	}
+
+
 
 	return tabbis;
 }
@@ -67,7 +79,7 @@ String.prototype.replaceLast = function(search, replace) {
 }
 
 function getTabbisStructure(itemPathListArray) {
-	const isDebug = true;
+	const isDebug = false;
 	const tabbisDataTabsArray = [];
 	const itemPathListArrayLength = itemPathListArray.length;
 
@@ -90,15 +102,15 @@ function getTabbisStructure(itemPathListArray) {
 									currentPathPreviousLevelFolderName = currentPathArray[level - 1].trim();
 									if ((0 == previousPathPreviousLevelFolderName.localeCompare(currentPathPreviousLevelFolderName))
 										&& 0 != ''.localeCompare(previousPathArray[level].trim())) {
-										tabbisDataTabsArray[level] += '<button class="button-tabbis"><span>' + currentPathArray[level] + '</span></button>';
+										tabbisDataTabsArray[level] += '<button class="button-tabbis" value="' + itemPathListArray[i] + '"><span>' + currentPathArray[level] + '</span></button>';
 										previousButtonName = currentPathArray[level].trim();
 									} else {
-										tabbisDataTabsArray[level] += '</div><div data-tabs><button class="button-tabbis"><span>' + currentPathArray[level] + '</span></button>';
+										tabbisDataTabsArray[level] += '</div><div data-tabs><button class="button-tabbis" value="' + itemPathListArray[i] + '"><span>' + currentPathArray[level] + '</span></button>';
 										previousButtonName = currentPathArray[level].trim();
 									}
 								}
 							} else {
-								tabbisDataTabsArray[level] = '<div data-tabs><button class="button-tabbis"><span>' + currentPathArray[level] + '</span></button>';
+								tabbisDataTabsArray[level] = '<div data-tabs><button class="button-tabbis" value="' + itemPathListArray[i] + '"><span>' + currentPathArray[level] + '</span></button>';
 								previousButtonName = currentPathArray[level].trim();
 							}
 						}
@@ -107,11 +119,11 @@ function getTabbisStructure(itemPathListArray) {
 							foundSubFolderInThisLevel = true;
 							if (typeof (tabbisDataTabsArray[0]) != "undefined") {
 								if (0 != previousButtonName.localeCompare(currentPathArray[0].trim())) {
-									tabbisDataTabsArray[0] += '<button class="button-tabbis"><span>' + currentPathArray[0] + '</span></button>';
+									tabbisDataTabsArray[0] += '<button class="button-tabbis" value="' + itemPathListArray[i] + '"><span>' + currentPathArray[0] + '</span></button>';
 									previousButtonName = currentPathArray[0].trim();
 								}
 							} else {
-								tabbisDataTabsArray[0] = '<div data-tabs><button class="button-tabbis"><span>' + currentPathArray[0] + '</span></button>';
+								tabbisDataTabsArray[0] = '<div data-tabs><button class="button-tabbis" value="' + itemPathListArray[i] + '"><span>' + currentPathArray[0] + '</span></button>';
 								previousButtonName = currentPathArray[level].trim();
 							}
 						}
@@ -141,4 +153,99 @@ function getTabbisStructure(itemPathListArray) {
 	return tabbisDataTabsArray;
 }
 
+function getKeyFromTabbisStructure(dataTabsInDataPanes, buttonIndex) {
+	head = 'value="';
+	tail = '"><span>';
+	let key = dataTabsInDataPanes[buttonIndex].substring(dataTabsInDataPanes[buttonIndex].indexOf(head) + head.length, dataTabsInDataPanes[buttonIndex].indexOf(tail));
+	return key;
+}
+
+function tuneTabbisStructure(tabbisDataTabsArray) {
+	const isDebug = false;
+	try {
+		for (i = tabbisDataTabsArray.length - 1; i > 0; i--) {
+			dataTabsInDataPanes = tabbisDataTabsArray[i].split('</div><div data-tabs>');
+			console.log("Current level: " + i);
+			for (j = 0; j < dataTabsInDataPanes.length; j++) {
+				// Find the 1st key:
+				//				head = 'value="';
+				//				tail = '"><span>';
+				//				let key = dataTabsInDataPanes[j].substring(dataTabsInDataPanes[j].indexOf(head) + head.length, dataTabsInDataPanes[j].indexOf(tail));
+				let key = getKeyFromTabbisStructure(dataTabsInDataPanes, j);
+				console.log("Current key value: " + key);
+				subKeyParts = key.split('/');
+				parentFolder = subKeyParts[subKeyParts.length - 3].trim();
+				console.log("Current parent folder name: " + parentFolder);
+				console.log("Find [[");
+
+				previousLevelDataTabsInDataPanes = tabbisDataTabsArray[i - 1].split('</div><div data-tabs>');
+				let bMatched = false;
+				for (l = 0; l < previousLevelDataTabsInDataPanes[l].length; l++) {
+					console.log("Previous Level Data Tabs In Data Panes [" + l + "]:" + previousLevelDataTabsInDataPanes[l]);
+
+					currentPreviousLevelDataTabsInDataPanes = previousLevelDataTabsInDataPanes[l].split('</button><button ');
+					for (k = 0; k < currentPreviousLevelDataTabsInDataPanes.length; k++) {
+						let curentBottonkey = getKeyFromTabbisStructure(currentPreviousLevelDataTabsInDataPanes, k);
+						subParts = curentBottonkey.split('/');
+						currentBottonFolderName = subParts[i - 1].trim();
+						console.log("Current key: " + curentBottonkey);
+						console.log("Current folder: " + currentBottonFolderName);
+						console.log("Current previous level: " + i - 1);
+						if (0 != parentFolder.localeCompare(currentBottonFolderName)) {
+							// TODO...
+
+						} else {
+							console.log("Matched!");
+
+							console.log("Refering current key: " + key);
+							console.log("Matched previous level button key: " + curentBottonkey);
+							console.log("Matched previous level tabbis index: " + l);
+							console.log("Matched previous level button key index: " + k);
+							console.log("Current level: " + i + "; Previous level: " + (i - 1));
+							console.log("Current level data tabbis index: " + j);
+
+							console.log(" ");
+							dataTabsPadding = '<div data-panes>';
+							if (0 == j) {
+								for (m = 0; m < k; m++) {
+									dataTabsPadding += '<div data-tabs>&nbsp;</div>';
+								}
+								tabbisDataTabsArray[i] = tabbisDataTabsArray[i].replace('<div data-panes>', dataTabsPadding);
+							} else {
+								// TODO...
+							}
+
+							bMatched = true;
+							break;
+						}
+					}
+
+					if (bMatched) break;
+
+					//					for (k = 0; k < previousLevelDataTabsInDataPanes.length; k++) {
+					//						let previousLevelKey = getKeyFromTabbisStructure(previousLevelDataTabsInDataPanes, k);
+					//						console.log("Previous level key: " + previousLevelKey);
+					//						previousLevelSubKeyParts = previousLevelKey.split('/');
+					//						previousLevelFolder = previousLevelSubKeyParts[previousLevelSubKeyParts.length - 2].trim();
+					//						if (0 != parentFolder.localeCompare(previousLevelFolder)) {
+					//							console.log("Previous level target folder: " + previousLevelFolder);
+					//
+					//							console.log("Current previous level: " + i - 1);
+					//						} else {
+					//							console.log("Matched!");
+					//							break;
+					//						}
+					//					}
+
+				}
+				console.log("]]");
+
+				// todo...
+			}
+		}
+	} catch (err) {
+		console.log('Error: ' + err.message);
+	}
+	return tabbisDataTabsArray;
+}
 
